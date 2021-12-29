243168d7c8d79d82c6ad9df5b148b2596ceaef1a
Montaniz Stills <Montaniz.Stills@vertexinc.com>
2021-12-29 10:52:41 -0500

    update Browsers.java -- ready for implementation; update demo notes in UseCaseTest.java

--- src/main/java/com/vertexinc/returns/test/cloudui/UseCaseTest.java
+++ src/main/java/com/vertexinc/returns/test/cloudui/UseCaseTest.java
@@ -3,22 +3,18 @@ package com.vertexinc.returns.test.cloudui;
 import com.vertexinc.returns.test.cloudui.util.Browsers;
 import com.vertexinc.returns.test.cloudui.util.DriverHandler;
 import com.vertexinc.returns.test.cloudui.util.Page;
-import io.github.bonigarcia.wdm.WebDriverManager;
 import org.junit.After;
 import org.junit.Assert;
 import org.junit.Test;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.edge.EdgeDriver;
-import org.openqa.selenium.safari.SafariDriver;
 
 
 /**
  * This framework aims to reduce future testing labor as tests (class UseCaseTest) implement behaviours of pre-defined pages.
  * The Test Suite is effective when calling upon the 'actions' of pre-defined web/site pages. AIM: Bridges gap between engineers.
  * This use case draws upon the behaviour of the PageInterface.
- * The PageInterface consumes a DriverHandler, who consumes a WebDriver.
- * This architecture aims to alleviate `mistakes` in testing, by enforcing strict type requirements. Such as <Test extends PageBehaviour>
+ * The Page consumes a DriverHandler, who consumes a WebDriver.
+ * This architecture aims to alleviate `mistakes` in testing, by enforcing strict type requirements and implicit waiting.
  **/
 
 public class UseCaseTest {
@@ -31,7 +27,7 @@ public class UseCaseTest {
     //    Test that Framework can open webpage. The demo webpage will be Vertex Corporate home page.
     public void Test_LoadCorporateWebPage() {
 
-        this.browser = Browsers.CHROME.getInstance();
+        this.browser = Browsers.EDGE.getInstance();
         this.driverHandler = new DriverHandler(getBrowser());
 
         //Given:
--- src/main/java/com/vertexinc/returns/test/cloudui/util/Browsers.java
+++ src/main/java/com/vertexinc/returns/test/cloudui/util/Browsers.java
@@ -8,19 +8,19 @@ import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.ie.InternetExplorerDriver;
 import org.openqa.selenium.opera.OperaDriver;
+import org.openqa.selenium.safari.SafariDriver;
 
 import java.util.function.Supplier;
 
-/**
- * NOT READY FOR IMPLEMENTATION. PLEASE DO NOT USE IN CODE
- **/
+
 public enum Browsers {
 
     FIREFOX(WebDriverManager::firefoxdriver, FirefoxDriver::new),
     CHROME(WebDriverManager::chromedriver, ChromeDriver::new),
     OPERA(WebDriverManager::operadriver, OperaDriver::new),
     IE(WebDriverManager::iedriver, InternetExplorerDriver::new),
-    EDGE(WebDriverManager::edgedriver, EdgeDriver::new);
+    EDGE(WebDriverManager::edgedriver, EdgeDriver::new),
+    SAFARI(WebDriverManager::safaridriver, SafariDriver::new);
 
     private final Supplier<WebDriver> webDriverSupplier;
     private final Supplier<WebDriverManager> webDriverManagerSupplier;
@@ -30,11 +30,11 @@ public enum Browsers {
         this.webDriverSupplier = webDriverSupplier;
     }
 
-    public Supplier<WebDriverManager> getWebDriverManagerSupplier() {
+    private Supplier<WebDriverManager> getWebDriverManagerSupplier() {
         return this.webDriverManagerSupplier;
     }
 
-    public Supplier<WebDriver> getWebDriverSupplier() {
+    private Supplier<WebDriver> getWebDriverSupplier() {
         return this.webDriverSupplier;
     }
 
