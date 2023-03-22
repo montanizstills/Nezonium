b5cd74215355f14ed2f03fbe3b384f5255a90e76
Montaniz.Stills <montanizstills@gmail.com>
2023-03-22 15:42:32 -0400

    update test to only support Firefox and Edge; update Pom.xml; deprecate Chrome; add surefire plugin;

--- src/test/java/io/github/nezonium/BrowserTest.java
+++ src/test/java/io/github/nezonium/BrowserTest.java
@@ -1,6 +1,7 @@
 package io.github.nezonium;
 
 import io.github.nezonium.annotations.UseDriver;
+import io.github.nezonium.util.DriverHandler;
 import io.github.nezonium.util.Page;
 import io.github.nezonium.util.SeleniumJupiterProvider;
 import io.github.nezonium.util.WebBrowser;
@@ -8,7 +9,7 @@ import org.junit.jupiter.api.Assertions;
 
 public class BrowserTest {
 
-    @UseDriver(browser = {WebBrowser.EDGE, WebBrowser.CHROME, WebBrowser.FIREFOX})
+    @UseDriver(browser = {WebBrowser.EDGE, WebBrowser.FIREFOX})
     public void should_Open_GoogleSearchEngine_Page(WebBrowser webBrowser) {
         SeleniumJupiterProvider.setUp(webBrowser); //embed in every test, accomplishable through inheritance? looks ugly siting there.
 
